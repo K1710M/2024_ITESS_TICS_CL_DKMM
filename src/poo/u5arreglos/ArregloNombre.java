0a2ccd0225020d6577e268b978051aa450553887
KarenMagana <maganak751@gmail.com>
2024-02-22 22:55:32 -0600

    POO Practica U5-1

---
+++ src/poo/u5arreglos/ArregloNombre.java
@@ -0,0 +1,17 @@
+
+package poo.u5arreglos;
+
+import java.util.Arrays;
+
+
+public class ArregloNombre {
+    public static void main (String [] args){
+        String nombres[] = {"Gerardo", "Paulina", "Nicólas", "Giovanni", "Alexis", "Leonardo", "Esteban", "Karen",
+        "Francisco", "Isaac", "Julian", "Antonio", "Gabriel", "Adrian", "Omar", "Manuel", "Alejandro"};
+        System.out.println(Arrays.toString(nombres));
+        
+        for(int i = 0; i<nombres.length; i++){
+            System.out.println(nombres[i]);
+        }
+    }
+}
---
+++ src/poo/u5arreglos/ArreglosN.java
@@ -0,0 +1,21 @@
+
+package poo.u5arreglos;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArreglosN {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        
+        System.out.print("Ingresa el tamaño del arreglo: ");
+        int a=leer.nextInt();
+        int p=0;
+        int num[] = new int [a];
+        
+         for(int i = 0; i<num.length; i++){
+            num[i]=leer.nextInt();
+        }
+         System.out.println(Arrays.toString(num));
+    }
+}

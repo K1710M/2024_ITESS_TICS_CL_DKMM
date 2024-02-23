0ec750f7b6ca3cecf5c87523fd56fb83e344fa09
KarenMagana <maganak751@gmail.com>
2024-02-23 15:25:50 -0600

    Se agrego las clases SumasRiemann, SumasEjemplo1, SumasTP03Ejercicio6

---
+++ src/ci/u1riemann/SumasRiemann.java
@@ -0,0 +1,51 @@
+/*
+ * ITESS-TICS
+ * SEMESTRE ENERO-JUNIO 2024
+ * CALCULO INTEGRAL
+ * UNIDAD 1: TEOREMA FUNDAMENTAL DE CALCULO
+ * CALCULO DE SERIES DE RIEMANN
+ * DIANA KAREN MANDUJANO MAGAÑA
+ * 23 / FEBRERO / 2024
+ */
+ 
+package ci.u1riemann;
+
+public class SumasRiemann {
+    protected double [] x;
+    protected double [] w;
+    protected double [] dx;
+    protected double [] fw;
+    protected double  suma;
+
+    public SumasRiemann(double[] x, double[] w) {
+        this.x = x;
+        this.w = w;
+        
+        //crear espacio de memoria
+        dx = new double[w.length];
+        fw = new double[w.length];
+        
+    }
+    
+    public void calcular() {
+        //calcular dx, fw, suma
+        suma = 0;
+        for(int i = 0; i < dx.length; i++){
+            dx[i] = x[i+1] - x[i];
+            fw[i] = funcion(w[i]);
+            suma += fw[i]*dx[i];
+        }
+    }
+    
+    public double funcion(double x) {
+        return 1.0;
+    }
+    
+    public void imprimir() {
+        System.out.println("i  |  wi  |  fw  |  fwi*dxi");
+        
+        for (int i  = 0; i<w.length; i++){
+            System.out.println(i + "  |  " + w[i] + "  |  " + fw[i] + "  |  " + fw[i]*dx[i]);
+        }
+    }
+}

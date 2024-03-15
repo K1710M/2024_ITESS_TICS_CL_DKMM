d8708e8ca08471a0a8e3208ea1d1633d6948573f
KarenMagana <maganak751@gmail.com>
2024-03-15 13:07:52 -0600

    add coordinate all

--- src/poo/u5arreglos/TI201Array.java
+++ src/poo/u5arreglos/TI201Array.java
@@ -1,16 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package poo.u5arreglos;
-
 import java.util.Arrays;
 import java.util.Scanner;
-
-/**
- *
- * @author Usuario
- */
 public class TI201Array {
     private Scanner leer;
     public static void main(String [] args ){
---
+++ src/tools/Coordinate.java
@@ -0,0 +1,84 @@
+/*
+ * ITESS-TICS
+ * SEMESTRE ENERO-JUNIO 2024
+ * CALCULO INTEGRAL
+ * CONVERTIR UN PUNTO A CO0RDENADAS PANTALLA
+ * DIANA KAREN MANDUJANO MAGAÑA
+ * 15 / MARZO / 2024
+ */
+package tools;
+
+public class Coordinate {
+   private double xmin; 
+   private double xmax; 
+   private double ymin; 
+   private double ymax; 
+   private double width; 
+   private double height; 
+
+    public Coordinate(double xmin, double xmax, double ymin, double ymax, double whidth, double hight) {
+        this.xmin = xmin;
+        this.xmax = xmax;
+        this.ymin = ymin;
+        this.ymax = ymax;
+        this.width = whidth;
+        this.height = hight;
+    }
+
+    public double getXmin() {
+        return xmin;
+    }
+
+    public void setXmin(double xmin) {
+        this.xmin = xmin;
+    }
+
+    public double getXmax() {
+        return xmax;
+    }
+
+    public void setXmax(double xmax) {
+        this.xmax = xmax;
+    }
+
+    public double getYmin() {
+        return ymin;
+    }
+
+    public void setYmin(double ymin) {
+        this.ymin = ymin;
+    }
+
+    public double getYmax() {
+        return ymax;
+    }
+
+    public void setYmax(double ymax) {
+        this.ymax = ymax;
+    }
+
+    public double getWhidth() {
+        return width;
+    }
+
+    public void setWhidth(double whidth) {
+        this.width = whidth;
+    }
+
+    public double getHight() {
+        return height;
+    }
+
+    public void setHight(double hight) {
+        this.height = hight;
+    }
+   
+   public double toScreenX(double xr){
+//       xs = (xr - xmin)* width / (xmax-xmin);
+       return (xr - xmin) * width / (xmax - xmin);
+   }
+   public double toScreenY(double yr){
+//       xs = height -(yr-ymin)*height / (ymax-ymin);
+       return height - (yr - ymin) * height / (ymax - ymin);
+   }
+}
